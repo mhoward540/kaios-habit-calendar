c4f70d6124b739cc692ea81f376552f61e078661
Matt Howard <mjhoward540@gmail.com>
2023-04-07 20:49:47 +0200

    get tailwind workinggit status

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@
 import {render} from 'solid-js/web';
 // import "tailwindcss/tailwind.css";
 
-// import './index.css';
+import './index.css';
 import App from './App';
 
 render(() => <App/>, document.body as HTMLElement);
